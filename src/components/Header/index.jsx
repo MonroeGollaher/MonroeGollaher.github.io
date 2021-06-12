e7f27b7e03d9a149f3e117b77b6d1d16ac526ef7
Monroe Gollaher <mgollaher92@gmail.com>
2021-06-11 22:34:17 -0600

    building

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { HeaderWrapper } from "./styles";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h3>Monroe Gollaher</h3>
+      <h5>Software Developer // Photographer</h5>
+      <p>
+        Full-stack software developer with a passion for creativity and problem
+        solving.
+      </p>
+      <p>
+        Javascript, C#, Dotnet Core, SQL, React, Vue.js, Node.js, MongoDB, Web
+        API's, CSS/Bootstrap, HTML5 & more.
+      </p>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  padding: 0 10px 10px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 0 10px 20px;
+`;
+
+export default {
+  HeaderWrapper,
+};
