5ce4054d645373289cbdd5b97f3bc3860ff537b4
MonroeGollaher <mgollaher92@gmail.com>
2023-08-15 22:16:54 -0600

    consolidate nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,76 @@
-import React from "react";
-import MobileNav from "./components/MobileNav";
-import DesktopNav from "./components/DesktopNav";
+import React, { useState } from "react";
 import styles from "./index.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ThemeToggle from "./components/ThemeToggle";
+import { string } from "prop-types";
+import { ThemeContext } from "../../App";
+import classNames from "class-names";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import navLinks from "./index.data";
 
-const Navbar = () => {
-  return (
-    <>
-      <DesktopNav className={styles.desktop} />
-      <MobileNav className={styles.mobile} />
-    </>
+const Navbar = ({ className }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeIcon = (props) => (
+    <svg
+      className={styles.mobileOnly}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      onClick={() => setMenuOpen(false)}
+      {...props}
+    >
+      <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+    </svg>
   );
+
+  const {
+    _currentValue: { theme },
+  } = ThemeContext;
+
+  return (
+    <div className={classNames(styles.component, className)}>
+      <div className={styles.menu}>
+        {menuOpen ? (
+          closeIcon()
+        ) : (
+          <FontAwesomeIcon
+            className={styles.mobileOnly}
+            icon={faBars}
+            size="lg"
+            onClick={() => setMenuOpen(true)}
+          />
+        )}
+        <div
+          className={classNames(
+            styles.nav,
+            menuOpen && styles.navOpen,
+            theme === "dark" && styles.darkNav
+          )}
+        >
+          <ul>
+            {navLinks.map(({ text, href }) => (
+              <li key={href}>
+                <a key={text} href={href}>
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <ThemeToggle />
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  className: string,
+};
+
+Navbar.defaultProps = {
+  className: "",
 };
 
 export default Navbar;
