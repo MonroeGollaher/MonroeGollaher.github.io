17b1847a920bcd0ceb2c93a0a6aee8bb02182cf2
MonroeGollaher <mgollaher92@gmail.com>
2024-01-15 22:27:06 -0700

    Make work history component

---
+++ src/components/Resume/index.data.js
@@ -0,0 +1,38 @@
+const jobData = [
+  {
+    company: "Foundry Interactive",
+    jobTitle: "Software Engineer",
+    dates: "Oct '23 - Current",
+    lineItems: [
+      "Software development in the real estate sector using React w/Redux.",
+      "Mixture of feature work and improving the clients website accessibility.",
+    ],
+  },
+  {
+    company: "MadeLabs",
+    jobTitle: "Software Engineer",
+    dates: "Nov '22 - Aug '23",
+    lineItems: [
+      "Software development in the financial tech space using React w/Redux and a Java backend.",
+      "Worked on different features and bug fixes for a large 20+ year old codebase.",
+      "Experience with Jenkins, IntelliJ, DBeaver and more.",
+      "Worked on internal projects using React & C#.",
+    ],
+  },
+  {
+    company: "Foundry Interactive",
+    jobTitle: "Software Engineer",
+    dates: "Apr '21 - Oct '22",
+    lineItems: [
+      "Software development using React, Typescript, Vite, Redux, Node.js, Express, Docker, Heroku, Postgress and more.",
+      "Assisted a start-up in the medical / health & beauty space launch an e-commerce website from the ground up.",
+      "Built React components / pages focused on scalability by utilizing a headless CMS.",
+      "Wrote unit and e2e tests using React Testing Library, Jest/Supertest as well as Playwright.",
+      "Used wire framing tools such as Figma to meet UX / UI schema acceptance criteria.",
+      "Assisted in development of open-source software for a medical company.",
+      "Took lead on migration to new Email Service Provider (ESP).",
+    ],
+  },
+];
+
+export default jobData;
