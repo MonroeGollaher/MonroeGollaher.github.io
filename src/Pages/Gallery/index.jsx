b216b017cdf4279aa9205581d3b796c69224030d
MonroeGollaher <mgollaher92@gmail.com>
2024-05-23 10:48:52 -0600

    Gallery consolidation and cleanup

--- src/Pages/Gallery/index.jsx
+++ src/Pages/Gallery/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styles from "./index.module.css";
-// import PhotoGallery from "../../components/PhotoGallery";
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
 export const GalleryPage = () => {
@@ -15,11 +14,20 @@ export const GalleryPage = () => {
     },
   });
 
+  const randomizeImages = (array) => {
+    array.forEach((_, i) => {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    });
+    return array;
+  };
+
   const fetchFileNames = async () => {
     try {
       const command = new ListObjectsV2Command({ Bucket: bucketName });
       const response = await s3.send(command);
-      const names = response.Contents?.map((item) => item.Key) || [];
+      let names = response.Contents?.map((item) => item.Key) || [];
+      names = randomizeImages(names);
       setFileNames(names);
     } catch (error) {
       console.error("Error fetching file names:", error);
@@ -29,12 +37,18 @@ export const GalleryPage = () => {
   useEffect(() => {
     fetchFileNames();
   }, []);
-  console.log(fileNames);
-
   return (
     <section className={styles.wrapper}>
       <h1>Photo Gallery</h1>
-      {/* <PhotoGallery /> */}
+      <div className={styles.gallery}>
+        {fileNames.map((fileName, index) => (
+          <img
+            key={fileName}
+            src={`https://${bucketName}.s3.amazonaws.com/${fileName}`}
+            alt={`Image ${index}`}
+          />
+        ))}
+      </div>
     </section>
   );
 };
